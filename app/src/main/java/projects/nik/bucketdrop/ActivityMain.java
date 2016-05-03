ec84050a5fbc0293acd6daf977ec15787deca6fc
Davenik <davenik24@gmail.com>
2016-05-03 17:28:54 +0530

    Added Bacground,button color,Handle button click event with Toast.

--- app/src/main/java/projects/nik/bucketdrop/ActivityMain.java
+++ app/src/main/java/projects/nik/bucketdrop/ActivityMain.java
@@ -1,13 +1,35 @@
 package projects.nik.bucketdrop;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
+
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        initBackgroundImage();
+
+
+    }
+
+    private void initBackgroundImage() {
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this).load(R.drawable.background).centerCrop().into(background);
+    }
+
+    public void showDialog(View view) {
+        Toast.makeText(this,"Button was Clicked",Toast.LENGTH_LONG).show();
     }
 }
